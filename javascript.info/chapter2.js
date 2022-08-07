2e3b1ddf5bc4d5971ad9210511e6ff83c94613a6
Lior Aharon <Lior610.aharon@gmail.com>
2022-08-07 15:51:43 +0300

    Basic Operathors and Operands

---
+++ javascript.info/chapter2.js
@@ -0,0 +1,36 @@
+"use strict";
+// Consts and vars decleration
+const OUR_PLANET = "earth";
+let isGreater = 4 > 1;
+let name = "john";
+
+// prompt, confirm and alert
+/* 
+let visitor = prompt("Enter your name: ", "Itay");
+let isBoss = confirm(`${visitor}, are you the boss?`);
+alert(isBoss); 
+*/
+
+// Conversion between data types
+/* 
+alert(Number("Hello")); // NaN
+alert(Boolean("0")); alert(Boolean(" ")); // true
+alert(Boolean(0)); alert(Boolean("")); // false 
+*/
+
+// Operands and Operators
+/*
+alert(typeof ('1' + 3));
+alert(+true);
+alert(+"");
+let num1 = +prompt("First number?", 1); //changes to number
+let num2 = +prompt("Second number?", 2); //changes to number
+alert(num1 + num2); // 12
+*/
+
+// Increments
+/*
+let a = 1, b = 1;
+let c = ++a; // 2
+let d = b++; // 1;
+*/
