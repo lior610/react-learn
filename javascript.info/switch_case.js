9c143119d87d31af3ae0d2e2d1a740fcae723a97
Lior Aharon <Lior610.aharon@gmail.com>
2022-08-08 22:38:21 +0300

    the switch statement

---
+++ javascript.info/switch_case.js
@@ -0,0 +1,52 @@
+/*
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Too small' );
+    break;
+  case 4:
+    alert( 'Exactly!' );
+    break;
+  case 5:
+    alert( 'Too big' );
+    break;
+  default:
+    alert( "I don't know such values" );
+}
+// no break
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Too small' );
+  case 4:
+    alert( 'Exactly!' );
+  case 5:
+    alert( 'Too big' );
+  default:
+    alert( "I don't know such values" );
+}
+
+let browser = "lior";
+if (browser == 'edge'){
+    alert("You've got the edge");
+} else if (browser == "chrome" || browser == "firefox" ||browser == "safari" ||browser == "opera") {
+    alert("We support it too");
+} else {
+    alert("We hope that this page looks ok!")
+}
+
+let a = +prompt('a?', '');
+switch(a) {
+    case (0):
+        alert(0);
+        break;
+    case(1):
+        alert(1);
+        break;
+    case(2):
+    case(3):
+        alert("2 or 3");
+        break;
+} */
